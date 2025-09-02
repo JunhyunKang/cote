ab2a7837a3be9f2eb4fc2d1507f46508cf68e662
JunhyunKang <kangjunhyun38@gmail.com>
2025-09-03 00:29:48 +0900

    0903_1

--- boj/1202.cpp
+++ boj/1202.cpp
@@ -2,14 +2,39 @@
 
 using namespace std;
 
-보석 n
-무개 m
-가격 v
-가방 k개, 최대무개 c
-
-
+int n, k;
+vector<pair<int, int>> bo;
+vector<int> bags;
 int main(void){
-
+    cin>>n>>k;
+    for(int i =0;i<n;i++){
+        int m,v;
+        cin>>m>>v;
+        bo.push_back({m,v});
+    }
+    sort(bo.begin(), bo.end());
+    for(int i =0;i<k;i++){
+        int m;
+        cin>>m;
+        bags.push_back(m);
+    }
     
+    sort(bags.begin(), bags.end());
+    long long cnt=0;
+    int idx = 0;
+    priority_queue<int> pq;
+    for(int i =0;i<k;i++){
+        while(idx<n && bo[idx].first <= bags[i]){
+
+            pq.push(bo[idx].second);
+            idx++;
+        }
+        if(!pq.empty()){
+            
+            cnt += pq.top();
+            pq.pop();
+        }
+    }
+    cout<<cnt;
     return 0;
 }

e7f76d1998405de5a6c3b0eb16e5d865922e0f1b
JunhyunKang <kangjunhyun38@gmail.com>
2025-09-10 01:56:07 +0900

    0909_1

--- boj/1753.cpp
+++ boj/1753.cpp
@@ -2,62 +2,50 @@
 
 using namespace std;
 
-int v,e,s;
+int v,e,k;
 
-
-
+vector<pair<int,int>> arr[20001];
+int d[20001];
 int main(void){
-    cin>>v>>e>>s;
+    cin>>v>>e>>k;
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    pq.push({0,k});
+    for(int i=0;i<e;i++){
+        int n1, n2, w;
+        cin>>n1>>n2>>w;
+        arr[n1].push_back({w,n2});
 
-    vector<vector<long long>> vec(v+1, vector<long long>(v+1,0));
-    vector<vector<int>> check(v+1, vector<int>(v+1,0));
-    vector<int> test[10];
-
-    for(long long i=0; i<e;i++){
-        long long x,y,va;
-        cin>>x>>y>>va;
-        if(vec[x][y]==0){
-            vec[x][y]=va;
-        }
-        else{
-            if(va<vec[x][y]){
-                vec[x][y]=va;
-            }
-        }
     }
-    queue<long long> q;
-    queue<long long> q2;
-    q.push(s);
-    q2.push(0);
-    check[s][s]=1;
-    while(!q.empty()){
-        long long cur = q.front();
-        long long dist = q2.front();
-        q2.pop();
-        q.pop();
-        for(long long i=1; i<v+1;i++){
-            if(vec[cur][i]!=0 && check[cur][i]==0){
-                check[cur][i]=1;
-                if(vec[s][i]==0||vec[s][i]>vec[cur][i]+dist){
-                    vec[s][i]=vec[cur][i]+dist;
+
+    while(!pq.empty()){
+        int value = pq.top().first;
+        int n = pq.top().second;
+        pq.pop();
+        
+        if(d[n]==value){
+
+            for(pair<int,int> p: arr[n]){
+                int w = p.first;
+                int n2 =p.second;
+                if(d[n2]==0||d[n2]>w+d[n]){
+                    d[n2]=w+d[n];
+                    pq.push({w+d[n],n2});
                 }
-                q.push(i);
-                q2.push(vec[s][i]);
+                
             }
         }
     }
-    
-    for(long long i=1; i<v+1;i++){
-        if(i==s){
-            cout<<0<<'\n';
-        }
-        else if(vec[s][i]!=0){
-            cout<<vec[s][i]<<'\n';
+    for(int i =1; i<v+1 ;i++){
+        if(i!=k && d[i]==0){
+            cout<<"INF"<<'\n';
         }
         else{
-            cout<<"INF\n";
+            cout<<d[i]<<'\n';
         }
-
     }
+
+
+
+    
     return 0;
 }
